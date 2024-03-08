1f760ecf4f62823a27e082b3f6534747a0516f76
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-03-08 17:34:28 +0200

    add student query

--- StudentQuery/Stored Procedure to See Exam Schedule of All Courses for a Student.sql
+++ StudentQuery/Stored Procedure to See Exam Schedule of All Courses for a Student.sql
@@ -4,28 +4,27 @@ CREATE OR ALTER PROCEDURE SeeExamesScheduleOfAllCourses
     @St_Id INT
 AS
 BEGIN
-    IF EXISTS (SELECT * FROM [dbo].[student] WHERE Std_Id = @St_Id) 
+    IF EXISTS (SELECT * FROM student WHERE Std_Id = @St_Id) 
     BEGIN
         DECLARE @Intake_Id INT
-        SELECT @Intake_Id = [Intake_Id] FROM [dbo].[student] WHERE Std_Id = @St_Id
+        SELECT @Intake_Id =   Intake_Id FROM student WHERE Std_Id = @St_Id
 
         IF @Intake_Id = 3
         BEGIN
-            -- Fetch exam details for all courses
             SELECT
-                CONCAT(S.Std_Fname, ' ', S.Std_Lname) AS [Student Name],
-                I.Intake_Name AS [Intake Year],
-                C.Crs_Name AS [Course Name],
-                E.Exam_Id AS [Exam ID],
-                E.E_Type AS [Exam Type],
-                E.Start_Time AS [Start Time],
-                E.End_Time AS [End Time],
-                E.Total_time AS [Total Time]
+                CONCAT(S.Std_Fname, ' ', S.Std_Lname) AS   StudentName  ,
+                I.Intake_Name AS IntakeYear  ,
+                C.Crs_Name AS CourseName  ,
+                E.Exam_Id AS ExamID  ,
+                E.E_Type AS ExamType  ,
+                E.Start_Time AS StartTime  ,
+                E.End_Time AS EndTime  ,
+                E.Total_time AS TotalTime  
             FROM 
-                [dbo].[student] S 
-                JOIN [dbo].[Intake] I ON I.Intake_Id = S.[Intake_Id]
-                JOIN [dbo].[Exam] E ON E.[Intake_Id] = I.[Intake_Id]
-                JOIN [dbo].[Course] C ON C.Crs_Id = E.Crs_Id 
+                    student   S 
+                JOIN Intake I ON I.Intake_Id = S.Intake_Id  
+                JOIN Exam E ON E.  Intake_Id = I.Intake_Id  
+                JOIN course C ON C.Crs_Id = E.Crs_Id 
             WHERE 
                 S.Std_Id = @St_Id;
         END
@@ -42,7 +41,6 @@ END
 
 
 
-EXEC SeeExamesScheduleOfAllCourses 1
----nvalid Student - Student Does Not Exist
+EXEC SeeExamesScheduleOfAllCourses 421
 EXEC SeeExamesScheduleOfAllCourses 600
 

48675859b803b26c63308796b2d5c895f8916bce
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-03-08 20:48:39 +0200

    add student query

--- instructorQuery/Setdegree.sql
+++ instructorQuery/Setdegree.sql
@@ -1,8 +1,9 @@
 CREATE OR ALTER PROCEDURE Set_Degree
-    @exam_id INT
+    @exam_id INT,
+    @course_id INT
 AS 
 BEGIN
-  -- check answers and set results
+    -- check answers and set results
     UPDATE seq
     SET seq.result = 
         CASE 
@@ -16,7 +17,7 @@ BEGIN
     WHERE 
         seq.Exam_Id = @exam_id;
 
-    -- set degree in student course table for all students
+    -- set degree in student course table for students in the specific course
     UPDATE sc
     SET sc.degree = ISNULL((
         SELECT SUM(seq.result)
@@ -24,9 +25,11 @@ BEGIN
         WHERE seq.Exam_Id = @exam_id AND seq.Std_Id = sc.Student_Id
         ), 0)
     FROM 
-        Student_Course sc;
+        Student_Course sc
+    WHERE 
+        sc.Course_Id = @course_id;
 
-	-- show updated table 
+    -- show updated table 
     SELECT 
         seq.Std_Id,
         seq.Exam_Id,
@@ -41,3 +44,4 @@ BEGIN
         seq.Exam_Id = @exam_id;
 END
 
+EXEC Set_Degree @exam_id = 1, @course_id = 1;
